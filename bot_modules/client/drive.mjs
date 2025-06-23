22ddb7e929c5351774973b12fbd6ba33072304a0
Prince-R20 <larrysamuel2020@gmail.com>
2025-06-23 13:59:48 +0100

    ensure proper adding of new line in private key

--- bot_modules/client/drive.mjs
+++ bot_modules/client/drive.mjs
@@ -4,11 +4,13 @@ import { google } from "googleapis";
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 export default async function getDriveClient() {
+  console.log(process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"));
+  console.log("Hello \n world")
   const credentials = {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"), // Ensure newlines are correctly formatted
   };
-  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n");
+  
   const auth = new google.auth.JWT(
     credentials.client_email,
     null,
