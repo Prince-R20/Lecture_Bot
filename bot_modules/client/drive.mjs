5321431283458aa8d158a1020538ec6ebd60fbb7
Prince-R20 <larrysamuel2020@gmail.com>
2025-07-15 11:47:33 +0100

    removed new line formatter

--- bot_modules/client/drive.mjs
+++ bot_modules/client/drive.mjs
@@ -4,8 +4,6 @@ import { google } from "googleapis";
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 export default async function getDriveClient() {
-  console.log(process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"));
-  console.log("Hello \n world")
   const credentials = {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     private_key: process.env.GOOGLE_PRIVATE_KEY, // Ensure newlines are correctly formatted
